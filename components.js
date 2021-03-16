b7709fe5f887e209b9432d3e1a7890fd7ffc7ac7
DAVID DOIL BREDESEN <daviddoil@gmail.com>
2021-03-15 23:46:30 -0500

    randomizer works

--- components.js
+++ components.js
@@ -9,23 +9,27 @@ const app = Vue.createApp({
   computed: {
     nameLength() {
       return this.first.length + this.last.length;
-    }
-  },
+    },
 
-  methods: {
     randomize() {
       let name = this.first + this.last;
       console.log(name);
       let nameArray = name.split("")
       console.log(nameArray);
-      let randomValue = Math.floor(Math.random() * (nameArray.length));
-      console.log(randomValue);
-      let randomName = [];
-      let randomNameValue = nameArray[randomValue];
-      randomName.push(randomNameValue);
-      console.log(randomName);
-      return randomName;
+      let nameArrayCopy = name.split("");
+      let randomNameArray = nameArray.map(rando => {
+        let randomValue = Math.floor(Math.random() * (nameArrayCopy.length));
+        rando = nameArrayCopy[randomValue];
+        nameArrayCopy.splice(randomValue, 1);
+        return rando;
+      })
+      let randomNameString = randomNameArray.join("")
+      return randomNameString;
     }
+  },
+
+  methods: {
+    
   }
   
 })
